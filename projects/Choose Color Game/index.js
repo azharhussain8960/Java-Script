c5dfe50c76cea13baa5c355115a5df4efaac5297
azhar hussain <hussainazhar8960@gmail.com>
2024-08-03 12:47:54 +0530

    ludu game

---
+++ projects/Choose Color Game/index.js
@@ -0,0 +1,9 @@
+
+ let ptag = document.getElementById("ptag")
+let inpt =  document.getElementsByTagName("inpt")
+   let heading = document.getElementsByClassName("he").innerHtml = "bye"
+   heading.innerHtml = "hy"
+console.log(ptag);
+console.log(inpt);
+console.log(heading);
+   
