0b7038a72ebc12a2062d2d4f15de05d4a69582ff
azhar hussain <hussainazhar8960@gmail.com>
2024-08-07 15:53:32 +0530

    add some code

---
+++ Classwork/Modules/app.js
@@ -0,0 +1,14 @@
+  
+
+export  let add = (a,b)=>{
+     return a + b
+  }
+    
+
+ export let colors =[ "red","blue","green"]
+for(let i =0 ; i<colors.length; i++){
+ console.log(colors[i]); 
+}
+  
+
+  export let z= 30
---
+++ Classwork/Modules/module.js
@@ -0,0 +1,10 @@
+import {z,add,colors} from "./app.js"
+
+let x= 20;
+console.log(x);
+
+console.log(add(200,300));
+
+
+
+
